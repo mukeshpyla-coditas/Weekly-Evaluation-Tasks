70855599a7f517d08232439aec66d071e9d6559d
mukesh.pyla <mukesh.pyla@coditas.com>
2026-04-06 12:06:15 +0530

    Commiting the AOP Task-2

--- src/aopTask/src/main/java/com/example/aopTask/entity/Resume.java
+++ src/aopTask/src/main/java/com/example/aopTask/entity/Resume.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,7 +20,11 @@ public class Resume {
     private Integer id;
 
     private Integer applicantId;
+    private String applicantName;
+    private String applicantEmail;
+    private List<String> skills;
+    private Integer experience;
+    private String description;
     private String fileType;
     private Double fileSize;
-    private String description;
 }
